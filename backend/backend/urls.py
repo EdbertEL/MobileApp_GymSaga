e020221ac7f6dcc1c57e2390dc7887ef3ed30ab5
EdbertEL <edbert.eldon.leopard@gmail.com>
2025-04-14 14:16:01 +0700

    Push admin user & views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib import admin  
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include API URLs
     path('auth/', include('authentication.urls')),  # Include Authentication URLs
 ]
